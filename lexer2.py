233d67b679236125aea9cbf0ef0f0cf54f0527d4
Anina <aninamimini@gmail.com>
2019-12-04 14:05:02 +0200

    correct fully functional
    "star" now gets an int (0-5) for the loop
    while(1): get input from user, print and run

--- lexer2.py
+++ lexer2.py
@@ -8,6 +8,10 @@ from myParser import getInfo
 @st.composite
 def chooseChar(draw, list):
 	return draw(st.characters(whitelist_categories=[], whitelist_characters=list))
+	
+@st.composite
+def randInt(draw):
+	return draw(st.integers(min_value=0, max_value=5))
 
 @st.composite
 def makeCorrect(draw, data):
@@ -15,18 +19,18 @@ def makeCorrect(draw, data):
 		return []
 	
 	#Star (zero or more instances) — *
-	elif len(data) > 1 and data[1] == "*":
+	if len(data) > 1 and data[1] == "*":
 		l = []
-		for i in range(5):		#to be fixed
+		for i in range(draw(randInt())):
 			l += draw(makeCorrect([data[0]]))
 		return l + draw(makeCorrect(data[2:]))
 	
 	#Grouping — ()
-	elif isinstance(data[0], list):
+	if isinstance(data[0], list):
 		return draw(makeCorrect(data[0]))
 		
 	#OR operator — []		
-	elif data[0] == "[":
+	if data[0] == "[":
 		if not "]" in data:
 			raise ValueError("Error: Unbalanced brackets")
 		end = data.index("]")	#get 1st instance of "["
@@ -35,7 +39,7 @@ def makeCorrect(draw, data):
 		#Star (zero or more instances) — *
 		if end + 1 < len(data) and data[end+1] == "*":
 			chosen = []
-			for i in range(5):	#to be fixed
+			for i in range(draw(randInt())):
 				chosen += [draw(chooseChar(data[1:end]))]
 			end2 += 1
 		else:
@@ -44,11 +48,38 @@ def makeCorrect(draw, data):
 		return chosen + draw(makeCorrect(data[(end2):]))
 		
 	#Rest aka just loose chars
-	else:
-		return [data[0]] + draw(makeCorrect(data[1:]))
-		
-@given(f=makeCorrect(['a','0','1','2','*']))
-def correct(f):
-	print("".join(f))
+	return [data[0]] + draw(makeCorrect(data[1:]))
 
-correct()
+@st.composite	
+def correct(draw, orig):
+	cor = draw(makeCorrect(data))
+	return("".join(cor))
+
+while (1):
+	orig = input("Give your reg expr: ")
+	print("Testing:", orig)
+	data = parse(orig)
+	print("Some correct inputs:")
+
+	@given(t=correct(orig))
+	def runCor(t):
+		#check if t matches the regexpr
+		assert re.match(r"^"+orig+"$", t), ("Generated string \"" + t + "\" does not match regexpr \"" + orig + "\"")
+		print(t)
+		
+	runCor()
+	
+	'''
+	info = getInfo(orig)
+	print("Some wrong inputs:")
+	
+	@given(t=wrong(orig))
+	def runWro(t):
+		print(t)
+		
+	runWro()
+	'''
+	
+	print()
+	
+
